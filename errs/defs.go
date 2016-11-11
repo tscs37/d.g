5f9fc7c65c04637fc54810fad7e11e1be7f9d48d
Tim Schuster <mail@timschuster.info>
2016-11-11 10:46:05 +0100

    Added new error messages

--- errs/defs.go
+++ errs/defs.go
@@ -7,4 +7,10 @@ var (
 	// be registered but another handler with the same name already
 	// exists
 	ErrHandlerNameDuplicate = errors.New("Duplicate Handler Name!")
+	// ErrHandlerNotSupported indicates that it was attempted to add
+	// a handler to a mux that doesn't support the specific handler type.
+	ErrHandlerNotSupported = errors.New("Handler not supported")
+	// ErrNotImplemnted indicates a particular function has not been implemented
+	// yet.
+	ErrNotImplemnted = errors.New("Function not implemented yet")
 )
